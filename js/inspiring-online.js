45bd3e6f36d3016ad4ab2ea8b2f64bf24fc5616a
Tim <timothy.w.holman@gmail.com>
2016-08-24 20:53:37 -0400

    initial tile grid

--- js/inspiring-online.js
+++ js/inspiring-online.js
@@ -1,3 +1,13 @@
 /* 
  *  Inspiring Online
  */
+
+var elem = document.querySelector('.tile-grid');
+var iso = new Isotope( elem, {
+  // options
+  itemSelector: '.tile',
+  masonry: {
+    gutter: 30,
+    fitWidth: true
+  }
+});
